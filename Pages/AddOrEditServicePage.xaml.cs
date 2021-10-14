a2bc93ae401491c2f12e39906f69e72c0d076934
Османов Никита <osmanov.nekit@mail.ru>
2021-10-14 12:51:16 +0300

    Добавьте файлы проекта.

---
+++ Pages/AddOrEditServicePage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System.IO;
+using System.Text;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.Win32;
+
+namespace CarServiceApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddOrEditServicePage.xaml
+    /// </summary>
+    public partial class AddOrEditServicePage : Page
+    {
+		Entities.Service currentService = null;
+		byte[] mainImageData;
+
+		public AddOrEditServicePage()
+        {
+            InitializeComponent();
+        }
+
+		public AddOrEditServicePage(Entities.Service service)
+		{
+			InitializeComponent();
+			currentService = service;
+			Title = "Редактировать услугу";
+			TBoxTitle.Text = currentService.Title;
+			TBoxCost.Text = currentService.Cost.ToString();
+			TBoxDuration.Text = (currentService.DurationInSeconds / 60).ToString();
+			TBoxDescription.Text = currentService.Description;
+			if (currentService.Discount > 0)
+				TBoxDiscount.Text = (currentService.Discount.Value * 100).ToString();
+			if (currentService.MainImage != null)
+				ImageService.Source = (ImageSource)new ImageSourceConverter().ConvertFrom(currentService.MainImage);
+		}
+			private void BtnSelectImage_Click(object sender, RoutedEventArgs e)
+		{
+			OpenFileDialog ofd = new OpenFileDialog();
+			ofd.Filter = "Image |*.png; *.jpg; *.jpeg *.tif";
+			if (ofd.ShowDialog() == true)
+			{
+				mainImageData = File.ReadAllBytes(ofd.FileName);
+				ImageService.Source = (ImageSource)new ImageSourceConverter().ConvertFrom(mainImageData);
+			}
+		}
+		private void BtnSave_Click(object sender, RoutedEventArgs e)
+		{
+			string errorMessage = CheckErrors();
+			if (errorMessage.Length > 0)
+				MessageBox.Show(errorMessage, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+			else
+			{
+				if (currentService == null)
+				{
+					App.Context.Services.Add(new Entities.Service
+					{
+						Title = TBoxTitle.Text,
+						Cost = decimal.Parse(TBoxCost.Text),
+						DurationInSeconds = int.Parse(TBoxDuration.Text) * 60,
+						Description = TBoxDescription.Text,
+						Discount = string.IsNullOrWhiteSpace(TBoxDiscount.Text) ? 0 : double.Parse(TBoxDiscount.Text) / 100,
+						MainImage = mainImageData
+					});
+					App.Context.SaveChanges();
+					MessageBox.Show("Услуга успешно добавлена.", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+				}
+				else
+				{
+					currentService.Title = TBoxTitle.Text;
+					currentService.Cost = decimal.Parse(TBoxCost.Text);
+					currentService.DurationInSeconds = int.Parse(TBoxDuration.Text) * 60;
+					currentService.Description = TBoxDescription.Text;
+					currentService.Discount = string.IsNullOrWhiteSpace(TBoxDiscount.Text) ? 0 : double.Parse(TBoxDiscount.Text) / 100;
+					if (mainImageData != null)
+						currentService.MainImage = mainImageData;
+					App.Context.SaveChanges();
+					MessageBox.Show("Услуга успешно отредактирована.", "Внимание", MessageBoxButton.OK, MessageBoxImage.Information);
+				}
+			}
+		}
+		private string CheckErrors()
+		{
+			var errorBuilder = new StringBuilder();
+
+			if (string.IsNullOrWhiteSpace(TBoxTitle.Text))
+				errorBuilder.AppendLine("Название услуги обязательно для заполнения;");
+
+			var serviceFromDB = App.Context.Services.ToList().FirstOrDefault(p => p.Title.ToLower() == TBoxTitle.Text.ToLower());
+			if (serviceFromDB != null && serviceFromDB != currentService)
+				errorBuilder.AppendLine("Такая услуга уже есть в базе данных;");
+			decimal cost = 0;
+			if (decimal.TryParse(TBoxCost.Text, out cost) == false || cost <= 0)
+				errorBuilder.AppendLine("Стоимость услуги должна быть положительным числом;");
+			int durationInMinutes = 0;
+			if (int.TryParse(TBoxDuration.Text, out durationInMinutes) == false || durationInMinutes > 240 || durationInMinutes <= 0)
+				errorBuilder.AppendLine("Длительность оказания услуги должна быть положительным числом (не больше, чем 4 часа);");
+			if (!string.IsNullOrEmpty(TBoxDiscount.Text))
+			{
+				int discount = 0;
+				if (int.TryParse(TBoxDiscount.Text, out discount) == false || discount < 0 || discount > 100)
+					errorBuilder.AppendLine("Размер скидки - целое число в диапазоне от 0 до 100%;");
+			}
+			if (errorBuilder.Length > 0)
+				errorBuilder.Insert(0, "Устраните следующие ошибки:\n");
+
+			return errorBuilder.ToString();
+		}
+	}
+}
---
+++ Pages/ServicesPage.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CarServiceApp.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ServicesPage.xaml
+    /// </summary>
+    public partial class ServicesPage : Page
+    {
+        public ServicesPage()
+        {
+            InitializeComponent();
+
+            BtnAddService.Visibility = App.CurrentUser.RoleId == 1 ? Visibility.Visible : Visibility.Collapsed;
+            LViewServices.ItemsSource = App.Context.Services.ToList();
+
+            ComboSortBy.SelectedIndex = 0;
+            ComboDiscount.SelectedIndex = 0;
+        }
+
+        private void BtnAddService_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddOrEditServicePage());
+        }
+        void BtnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new AddOrEditServicePage((sender as Button).DataContext as Entities.Service));
+        }
+
+        void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var currentService = (sender as Button).DataContext as Entities.Service;
+            if (MessageBox.Show($"Вы уверены, что хотите удалить услугу: {currentService.Title}?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                App.Context.Services.Remove(currentService);
+                App.Context.SaveChanges();
+                UpdateServices();
+            }
+        }
+
+        private void ComboSortBy_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+        
+        private void ComboDiscount_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateServices();
+        }
+
+        void UpdateServices()
+        {
+            var services = App.Context.Services.ToList();
+            int total = services.Count;
+            if (ComboSortBy.SelectedIndex == 0)
+                services = services.OrderBy(p => p.CostWithDiscount).ToList();
+            else
+                services = services.OrderByDescending(p => p.CostWithDiscount).ToList();
+            switch (ComboDiscount.SelectedIndex)
+            {
+                case 1: services = services.Where(p => p.Discount >= 0 && p.Discount < 0.05).ToList(); break;
+                case 2: services = services.Where(p => p.Discount >= 0.05 && p.Discount < 0.15).ToList(); break;
+                case 3: services = services.Where(p => p.Discount >= 0.15 && p.Discount < 0.30).ToList(); break;
+                case 4: services = services.Where(p => p.Discount >= 0.30 && p.Discount < 0.70).ToList(); break;
+                case 5: services = services.Where(p => p.Discount >= 0.70 && p.Discount <= 1).ToList(); break;
+            }
+            services = services.Where(p => p.Title.ToLower().Contains(TBoxSearch.Text.ToLower())).ToList();
+            LViewServices.ItemsSource = services;
+            BlockRecords.Text = $"{services.Count} из {total}";
+        }
+    }
+}
